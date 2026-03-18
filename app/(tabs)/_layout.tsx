2dfd311fc367a70e6e0058884aa30a7c6d9b3abd
yangsooplus <yangsooplus@naver.com>
2026-03-18 22:32:48 +0900

    feat: 바텀 네비게이션 아이콘 설정

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,29 @@
 import {Tabs} from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function TabLayout() {
-    return <Tabs>
-        <Tabs.Screen name="map" options={{title: "쉬크릿 지도"}}/>
-        <Tabs.Screen name="my-shhhcret" options={{title: "마이 쉬크릿"}}/>
+    return <Tabs screenOptions={{
+        tabBarActiveTintColor: "#000000",
+        tabBarInactiveTintColor: "#94A3B8",
+    }}>
+        <Tabs.Screen
+            name="map"
+            options={{
+                title: "쉬크릿 지도",
+                tabBarLabel: "지도",
+                tabBarIcon: ({color, size, focused}) => (
+                    <Ionicons name={focused ? "map" : "map-outline"} size={size} color={color}/>
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="my-shhhcret"
+            options={{
+                title: "마이 쉬크릿",
+                tabBarIcon: ({color, size, focused}) => (
+                    <Ionicons name={focused ? "person" : "person-outline"} size={size} color={color}/>
+                ),
+            }}
+        />
     </Tabs>;
 }
